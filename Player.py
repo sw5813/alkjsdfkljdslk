541e5a33217765e9ffefd6eab193ac9fd0bbd5c0
sw5813 <sw5813@gmail.com>
2013-08-18 20:46:50 -0500

    dysfunctional tracking

--- Player.py
+++ Player.py
@@ -31,13 +31,17 @@ class Player(BasePlayer):
         global players
         players = len(player_reputations)
 
+        #for use in tracking in hunt_outcomes
+        global reps
+        reps = list(player_reputations)
+
         sorted_player_reps = sorted(player_reputations)
         hunt_decisions = []
         dictionary = {}
-        global tracking_dict
-        tracking_dict = {}  
-        location = {}
+        matched = []
+        decision = []
 
+        #Setting the variables in round 1
         if round_number == 1:
             self.strategy = 'blank'
             self.hunt_cap = 0
@@ -48,36 +52,42 @@ class Player(BasePlayer):
             global h
             h = 0
 
+        #This section which results in tit for tat is run when self.tracking is set to true; the default from round 1 is false
         if self.tracking:
-            print("TRACKING IN PROGRESS: PLAYER REPUTATIONS")
-            index = 1
-            for rep in player_reputations:
-                tracking_dict[index] = rep
-                index += 1
-            for x in range(1, len(tracking_dict)+1):
-                for y in range(1, len(tracking_dict_copy)+1):
-                    if tracking_dict_copy[y] == tracking_dict[x]:
-                        location[y] = tracking_dict[x]
+            print("TRACKING IN PROGRESS")
+            for x in tracking_reputations:
+                for y in reps:
+                    if abs(x - y) < 0.001:
+                        matched.append(y)
+            for x in tracking_responses:
+                if tracking_responses == 's':
+                    decision.append('h')
+                else: 
+                    decision.append('s') 
+            for index in range(1,len(reps)+1):
+                dictionary[matched[index]] = decision[index]
 
-        for rep in sorted_player_reps:
-            dictionary[rep] = 's'
-
-        if self.strategy == 'slack':
-            dictionary[rep] = ['s' for rep in player_reputations]
         else:
+            #The default decision is to always slack. These decisions are put in a dictionary.
             for rep in sorted_player_reps:
-                if rep > round_number / 75:
-                    for r in range(1,int(self.avg_hunts+1)):         
-                        dictionary[sorted_player_reps[len(sorted_player_reps)-r]] = 'h'
-        
+                dictionary[rep] = 's'
+
+            if self.strategy == 'slack':
+                dictionary[rep] = ['s' for rep in player_reputations]
+            else:
+                for rep in sorted_player_reps:
+                    if rep > round_number / 75:             #hunts with an increasingly narrow range of players with high reputations
+                        for r in range(1,int(self.avg_hunts+1)):         
+                            dictionary[sorted_player_reps[len(sorted_player_reps)-r]] = 'h'
+            
+            #The final step is to transfer the reputations from the dictionary to the list "hunt_decisions" for submission
         for rep in player_reputations:
             hunt_decisions.append(dictionary[rep])
 
         return hunt_decisions
 
-    def hunt_outcomes(self, food_earnings): #IMPLEMENT HUNT_CAP
-        tracking_dict2 = {}
-
+    def hunt_outcomes(self, food_earnings): 
+        #Using food_earnings, it's possible to figure out what each player I hunted with chose to do
         successful_hunts=food_earnings.count(0)
         failed_hunts=food_earnings.count(-3)
         my_hunts=successful_hunts+failed_hunts
@@ -88,37 +98,41 @@ class Player(BasePlayer):
 
         opponents_hunt = successful_hunts + successful_slacks
         
+        #If the players I hunted with hunted more often than I did, I should hunt less, specifically approximately how often they did
         if opponents_hunt > my_hunts:
             self.hunt_cap = opponents_hunt
 
+        #When each round barely impacts reputation, I begin to track my opponents
         global total_hunts
         total_hunts += players
-        print(total_hunts)
         global h
         h += my_hunts
-        print(h)
-        if (h+players)/(total_hunts+players)-h/total_hunts<.001:
+        x=((h+players)/(total_hunts+players))-(h/total_hunts)
+        print 'change in rep=', x 
+        print 'players=', players
+        print 'h=',h
+        print 'total_hunts=',total_hunts
+        if ((h + players)/(total_hunts+players))-(h/total_hunts) < .001:
             self.tracking = True
+            print("START TRACKING NEXT ROUND")
 
         if self.tracking:
-            index = 1
+            global tracking_responses
+            tracking_responses = []
             for food in food_earnings:
                 if food == 0 or food == 1:
-                    tracking_dict2[index] = 'h'
+                    tracking_responses.append('h')
                 else:
-                    tracking_dict2[index] = 's'
-                index += 1
-            global tracking_dict_copy
-            tracking_dict_copy = dict(tracking_dict)
-            print("TRACKING: OPPONENT REPUTATIONS")
-            print tracking_dict_copy
-            print("TRACKING: OPPONENT ACTION")
-            print(tracking_dict2)
+                    tracking_responses.append('s')
+            global tracking_reputations
+            tracking_reputations = list(reps)
 
     def round_end(self, award, m, number_hunters):
+        #If nobody hunted in the last round, I should stop hunting
         if number_hunters == 0:
             self.strategy = 'slack'
 
+        #This determines the average amount of times players hunted in the last round, and in the next round I set that as the cap for my hunting so that I'm not overhunting. If everyone else's reputation is very low, I don't want mine to be too much higher.
         if players > 1:
             self.avg_hunts = round(number_hunters/(players*(players-1)))
         else:
